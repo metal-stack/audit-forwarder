deab8a998237acb06087977066fb8b4e3e18883d
iz00242 <michael.reiger@f-i-ts.de>
2020-12-04 19:06:58 +0100

    Changed parametrisation

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/mreiger/audit-tailer-controller
 go 1.13
 
 require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/metal-stack/v v1.0.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
 	go.uber.org/zap v1.16.0
+	gopkg.in/go-playground/validator.v9 v9.31.0
 	k8s.io/api v0.19.4
 	k8s.io/apimachinery v0.19.4
 	k8s.io/client-go v0.19.4
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"time"
 
+	"log"
+
 	"go.uber.org/zap"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -13,6 +15,7 @@ import (
 	"context"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/metal-stack/v"
@@ -20,27 +23,90 @@ import (
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 const (
+	cfgFileType = "yaml"
 	moduleName  = "audit-forwarder"
-	namespace   = "kube-system"
-	podname     = "kubernetes-audit-tailer"
-	podport     = "24224"
 	commandName = "/fluent-bit/bin/fluent-bit"
 	commandArgs = "--config=/fluent-bit/etc/fluent-bit.conf"
 )
 
-var rootCmd = &cobra.Command{
+var (
+	cfgFile string
+	logger  *zap.SugaredLogger
+)
+
+// Opts is required in order to have proper validation for args from cobra and viper.
+// this is because MarkFlagRequired from cobra does not work well with viper, see:
+// https://github.com/spf13/viper/issues/397
+type Opts struct {
+	KubeCfg       string
+	FetchInterval time.Duration
+	NameSpace     string
+	ServiceName   string
+	ServicePort   int
+	LogLevel      string
+}
+
+var cmd = &cobra.Command{
 	Use:     moduleName,
-	Short:   "a service that watches for an audit-tailer pod in the cluster and then starts the audit-forwarder with the right destination IP",
+	Short:   "a webhook that accepts audit events and writes them to stdout so they can be picked up by another log processing system.",
 	Version: v.V.String(),
 	Run: func(cmd *cobra.Command, args []string) {
-		run()
+		initConfig()
+		opts, err := initOpts()
+		if err != nil {
+			log.Fatalf("unable to init options, error: %v", err)
+		}
+		initLogging()
+		run(opts)
 	},
 }
 
-var logger *zap.SugaredLogger
+func init() {
+	log.Print("Function init() called.")
+	homedir, err := homedir.Dir()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	log.Printf("Homedir variable homedir: %s", homedir)
+
+	cmd.Flags().StringP("log-level", "", "info", "sets the application log level")
+	cmd.Flags().StringVarP(&cfgFile, "config", "c", "", "alternative path to config file")
+
+	cmd.Flags().StringP("kubecfg", "k", homedir+"/.kube/config", "kubecfg path to the cluster to account")
+	cmd.Flags().Duration("fetch-interval", 10*time.Second, "interval for checking availability of target")
+
+	cmd.Flags().StringP("namespace", "", "kube-system", "the namespace of the audit-tailer service")
+	cmd.Flags().StringP("service-name", "", "kubernetes-audit-tailer", "the service name of the audit-tailer service")
+	cmd.Flags().IntP("service-port", "", 24224, "the service port of the audit-tailer service")
+
+	err = viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		logger.Fatalw("unable to construct root command", "error", err)
+	}
+}
+
+func initOpts() (*Opts, error) {
+	opts := &Opts{
+		KubeCfg:       viper.GetString("kubecfg"),
+		FetchInterval: viper.GetDuration("fetch-interval"),
+		NameSpace:     viper.GetString("namespace"),
+		ServiceName:   viper.GetString("service-name"),
+		ServicePort:   viper.GetInt("service-port"),
+		LogLevel:      viper.GetString("log-level"),
+	}
+
+	validate := validator.New()
+	err := validate.Struct(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return opts, nil
+}
 
 func main() {
 	zap, _ := zap.NewProduction()
@@ -48,33 +114,73 @@ func main() {
 		_ = zap.Sync()
 	}()
 	logger = zap.Sugar()
-	if err := rootCmd.Execute(); err != nil {
+	if err := cmd.Execute(); err != nil {
 		logger.Error("Failed executing root command", "Error", err)
 	}
 }
 
-func init() {
+func initConfig() {
 	viper.SetEnvPrefix("audit")
-	homedir, err := homedir.Dir()
-	if err != nil {
-		logger.Fatal(err)
-	}
-	rootCmd.PersistentFlags().StringP("kubecfg", "k", homedir+"/.kube/config", "kubecfg path to the cluster to account")
-	rootCmd.PersistentFlags().Duration("fetch-interval", 10*time.Second, "interval for checking availability of target")
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
-	err = viper.BindPFlags(rootCmd.PersistentFlags())
-	if err != nil {
-		logger.Fatal(err)
+
+	viper.SetConfigType(cfgFileType)
+
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		if err := viper.ReadInConfig(); err != nil {
+			logger.Errorw("Config file path set explicitly, but unreadable", "error", err)
+			os.Exit(1)
+		}
+	} else {
+		viper.SetConfigName("config")
+		viper.AddConfigPath("/etc/" + moduleName)
+		viper.AddConfigPath("$HOME/." + moduleName)
+		viper.AddConfigPath(".")
+		if err := viper.ReadInConfig(); err != nil {
+			usedCfg := viper.ConfigFileUsed()
+			if usedCfg != "" {
+				logger.Errorw("Config file unreadable", "config-file", usedCfg, "error", err)
+				os.Exit(1)
+			}
+		}
+	}
+
+	usedCfg := viper.ConfigFileUsed()
+	if usedCfg != "" {
+		logger.Infow("Read config file", "config-file", usedCfg)
 	}
 }
 
-func run() {
-	client, err := loadClient(viper.GetString("kubecfg"))
+func initLogging() {
+	level := zap.InfoLevel
+
+	if viper.IsSet("log-level") {
+		err := level.UnmarshalText([]byte(viper.GetString("log-level")))
+		if err != nil {
+			log.Fatalf("can't initialize zap logger: %v", err)
+		}
+	}
+
+	cfg := zap.NewProductionConfig()
+	cfg.Level = zap.NewAtomicLevelAt(level)
+
+	log.Printf("Log level: %s", cfg.Level)
+
+	l, err := cfg.Build()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+
+	logger = l.Sugar()
+}
+
+func run(opts *Opts) {
+	logger.Infow("Options", "opts", opts)
+	client, err := loadClient(opts.KubeCfg)
 	if err != nil {
 		logger.Fatalw("Unable to connect to k8s", "Error", err)
 	}
-
-	fetchInterval := viper.GetDuration("fetch-interval")
 
 	// Apparently we now need to pass a context co k8s client API
 	kubectx, kubecancel := context.WithCancel(context.Background())
@@ -88,12 +194,12 @@ func run() {
 
 	logger.Infow("Initial old values", "oldPodIP", oldPodIP, "oldPodDate", oldPodDate)
 
-	labelMap := map[string]string{"app": podname}
-	opts := metav1.ListOptions{
+	labelMap := map[string]string{"app": opts.ServiceName}
+	options := metav1.ListOptions{
 		LabelSelector: labels.SelectorFromSet(labelMap).String(),
 	}
 	for {
-		watcher, err := client.CoreV1().Pods(namespace).Watch(kubectx, opts)
+		watcher, err := client.CoreV1().Pods(opts.NameSpace).Watch(kubectx, options)
 		if err != nil {
 			logger.Fatalw("Could not watch for pods", "Error", err)
 		}
@@ -129,7 +235,7 @@ func run() {
 							cmd.Stderr = os.Stderr
 							cmd.Env = append(os.Environ(),
 								"AUDIT_TAILER_HOST="+podIP,
-								"AUDIT_TAILER_PORT="+podport,
+								"AUDIT_TAILER_PORT="+strconv.Itoa(opts.ServicePort),
 							)
 							logger.Infow("Executing:", "Command", strings.Join(cmd.Args, " "), ", Environment:", strings.Join(cmd.Env, ", "))
 
@@ -145,7 +251,7 @@ func run() {
 								return
 							default:
 								logger.Infow("Forwarder was not killed by this controller, restarting")
-								time.Sleep(fetchInterval)
+								time.Sleep(opts.FetchInterval)
 							}
 						}
 					}()
