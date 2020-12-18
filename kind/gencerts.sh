43e6d0bd3ab810ca548e1da422bdd55db3e89471
iz00242 <michael.reiger@f-i-ts.de>
2020-12-18 12:57:28 +0100

    Improved kind cluster certificate handling and setup scripts

--- kind/gencerts.sh
+++ kind/gencerts.sh
@@ -10,7 +10,6 @@ set -o nounset
 set -o pipefail
 
 CERTDIR=certs
-# MANIFESTSDIR=../deployment
 
 # CREATE THE PRIVATE KEY FOR OUR CUSTOM CA
 openssl genrsa -out $CERTDIR/ca.key 2048
@@ -19,25 +18,42 @@ openssl genrsa -out $CERTDIR/ca.key 2048
 openssl req -new -x509 -key $CERTDIR/ca.key -out $CERTDIR/ca.crt -config $CERTDIR/ca-config.txt
 
 # CREATE THE PRIVATE KEY FOR OUR fluentd SERVER
-openssl genrsa -out $CERTDIR/fluentd-key.pem 2048
+openssl genrsa -out $CERTDIR/tailer.key 2048
 
 # CREATE A CSR FROM THE CONFIGURATION FILE AND OUR PRIVATE KEY
-openssl req -new -key $CERTDIR/fluentd-key.pem -subj "/CN=kubernetes-audit-tailer" -out $CERTDIR/fluentd.csr -config $CERTDIR/ca-config.txt
+openssl req -new -key $CERTDIR/tailer.key -subj "/CN=kubernetes-audit-tailer" -out $CERTDIR/tailer.csr -config $CERTDIR/ca-config.txt
 
 # CREATE THE CERT SIGNING THE CSR WITH THE CA CREATED BEFORE
-openssl x509 -req -in $CERTDIR/fluentd.csr -CA $CERTDIR/ca.crt -CAkey $CERTDIR/ca.key -CAcreateserial -out $CERTDIR/fluentd-crt.pem
+openssl x509 -req -in $CERTDIR/tailer.csr -CA $CERTDIR/ca.crt -CAkey $CERTDIR/ca.key -CAcreateserial -out $CERTDIR/tailer.crt
 
 # CREATE THE PRIVATE KEY FOR OUR forwarder
-openssl genrsa -out $CERTDIR/forwarder-key.pem 2048
+openssl genrsa -out $CERTDIR/forwarder.key 2048
 
 # CREATE A CSR FROM THE CONFIGURATION FILE AND OUR PRIVATE KEY
-openssl req -new -key $CERTDIR/forwarder-key.pem -subj "/CN=kubernetes-audit-forwarder" -out $CERTDIR/forwarder.csr -config $CERTDIR/ca-config.txt
+openssl req -new -key $CERTDIR/forwarder.key -subj "/CN=kubernetes-audit-forwarder" -out $CERTDIR/forwarder.csr -config $CERTDIR/ca-config.txt
 
 # CREATE THE CERT SIGNING THE CSR WITH THE CA CREATED BEFORE
-openssl x509 -req -in $CERTDIR/forwarder.csr -CA $CERTDIR/ca.crt -CAkey $CERTDIR/ca.key -CAcreateserial -out $CERTDIR/forwarder-crt.pem
+openssl x509 -req -in $CERTDIR/forwarder.csr -CA $CERTDIR/ca.crt -CAkey $CERTDIR/ca.key -CAcreateserial -out $CERTDIR/forwarder.crt
 
-# INJECT CA IN THE WEBHOOK CONFIGURATION
-export CA_BUNDLE=$(cat $CERTDIR/ca.crt | base64 | tr -d '\n')
+# Create certificate secret manifests for tailer. In the kind cluster, the forwarder needs them copied into the file system because accessing secrets does not work at apiserver creation time.
+CA_KEY=$(cat $CERTDIR/ca.key | base64 | tr -d '\n')
+CA_BUNDLE=$(cat $CERTDIR/ca.crt | base64 | tr -d '\n')
+TAILER_KEY=$(cat $CERTDIR/tailer.key | base64 | tr -d '\n')
+TAILER_CERT=$(cat $CERTDIR/tailer.crt | base64 | tr -d '\n')
+FORWARDER_KEY=$(cat $CERTDIR/forwarder.key | base64 | tr -d '\n')
+FORWARDER_CERT=$(cat $CERTDIR/forwarder.crt | base64 | tr -d '\n')
 
-# CABundle is not inserted in file here, instead it must be mounted - see https://kubernetes.io/docs/reference/access-authn-authz/webhook/#configuration-file-format
-# cat $MANIFESTSDIR/token-webhook-config.tpl | envsubst > $MANIFESTSDIR/token-webhook-config.yaml
+cat <<EOF >$CERTDIR/tailer-certs.yaml
+---
+apiVersion: v1
+data:
+  ca.key: $CA_KEY
+  ca.crt: $CA_BUNDLE
+  tailer.key: $TAILER_KEY
+  tailer.crt: $TAILER_CERT
+kind: Secret
+metadata:
+  name: tailer-certs
+  namespace: kube-system
+type: Opaque
+EOF
